636298b86f43cfcff716524514da18b315d2d526
Martuko <martin.aguilera@mail.udp.cl>
2025-06-17 04:36:36 +0000

    lab_5 cripto

--- Cripto/Cripto_Lab/Lab_5/hassh.sh
+++ Cripto/Cripto_Lab/Lab_5/hassh.sh
@@ -2,15 +2,10 @@
 
 PCAP="$1"
 
-FIELDS=$(tshark -r "$PCAP" -Y "ssh.kex_algorithms && ip.src==192.168.200.5" -T fields \
-  -e ssh.kex_algorithms \
-  -e ssh.server_host_key_algorithms \
-  -e ssh.encryption_algorithms_client_to_server \
-  -e ssh.mac_algorithms_client_to_server \
-  -e ssh.compression_algorithms_client_to_server)
+ALGORITHMS=$(tshark -r "$PCAP" -Y "ssh.kex_algorithms && ip.src==192.168.200.4" -T fields -e ssh.kex.hassh_algorithms)
+HASH=$(tshark -r "$PCAP" -Y "ssh.kex_algorithms && ip.src==192.168.200.4" -T fields -e ssh.kex.hassh)
 
-HASSH_STRING=$(echo "$FIELDS" | paste -sd ";" -)
-echo "HASSH String:"
-echo "$HASSH_STRING"
-echo -n "$HASSH_STRING" | md5sum
-
+echo "HASSH Algorithms:"
+echo "$ALGORITHMS"
+echo "HASSH MD5:"
+echo "$HASH"
