e0eddde298008db4ec4400786d283fae5e0c6a53
Martuko <martin.aguilera@mail.udp.cl>
2025-06-19 07:11:26 +0000

    lab_5 cripto

--- Cripto/Cripto_Lab/Lab_5/hassh.sh
+++ Cripto/Cripto_Lab/Lab_5/hassh.sh
@@ -2,8 +2,8 @@
 
 PCAP="$1"
 
-ALGORITHMS=$(tshark -r "$PCAP" -Y "ssh.kex_algorithms && ip.src==192.168.200.4" -T fields -e ssh.kex.hassh_algorithms)
-HASH=$(tshark -r "$PCAP" -Y "ssh.kex_algorithms && ip.src==192.168.200.4" -T fields -e ssh.kex.hassh)
+ALGORITHMS=$(tshark -r "$PCAP" -Y "ssh.kex_algorithms && ip.src==192.168.200.10" -T fields -e ssh.kex.hassh_algorithms)
+HASH=$(tshark -r "$PCAP" -Y "ssh.kex_algorithms && ip.src==192.168.200.10" -T fields -e ssh.kex.hassh)
 
 echo "HASSH Algorithms:"
 echo "$ALGORITHMS"
