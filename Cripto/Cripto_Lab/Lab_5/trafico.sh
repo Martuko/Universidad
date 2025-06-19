b04384c058c4c23bbe54af5bc62c8da8483e756c
Martuko <martin.aguilera@mail.udp.cl>
2025-06-19 07:12:18 +0000

    lab_5 cripto

--- Cripto/Cripto_Lab/Lab_5/trafico.sh
+++ Cripto/Cripto_Lab/Lab_5/trafico.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
-# Uso: ./trafico.sh archivo.pcapng
-
 PCAP="$1"
 
 echo "========== Tamaño de paquetes del flujo =========="
 tshark -r "$PCAP" -Y "tcp.port == 22" -T fields -e frame.number -e frame.len -e ip.src -e ip.dst
 
 echo ""
-echo "========== Contenido visible en texto plano (si lo hay) =========="
-tshark -r "$PCAP" -Y "tcp.port == 22 && data" -T fields -e ip.src -e ip.dst -e data.data
+
